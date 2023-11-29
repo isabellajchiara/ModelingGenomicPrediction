67021ac7ba2e798f68a2d382a3f39c0ac31a2039
Isabella C. Penney <95446790+isabellajchiara@users.noreply.github.com>
2023-11-29 14:32:56 -0500

    create tensor that breaks down genotype by ploidy

--- formatData.py
+++ formatData.py
@@ -1,18 +1,29 @@
-geno = pd.read_csv("unstructuredGeno.csv")
-resp = pd.read_csv("unstructuredPheno.csv")
-data = resp.join(geno)
+geno = pd.read_csv("simHaplo.csv")
+geno = pd.DataFrame(geno.drop('Unnamed: 0', axis=1))
 
-nLoci = 3570
-nInd = len(geno0/2
+resp = pd.read_csv("simYieldData.csv")
+resp = resp.drop('Unnamed: 0', axis=1)
+
+nLoci = int(len(geno.columns))
+nInd = int(len(geno)/2)
 ploidy = 2
 
-train = torch.tensor(data.values, dtype=torch.float)
-x = train[:,0]
-geno3D = X.reshape(nLoci, nInd,ploidy)
-y = train[:,1:3548]
+chr2 = np.empty([nInd, nLoci], dtype=int)
+chr1 = np.empty([nInd, nLoci], dtype=int)
+x = 1
+
+while x < (len(geno)-2):
+    c1 = geno.iloc[x:(x+1),:] # pull row 1
+    c2 = geno.iloc[(x+1):(x+2),:] #pull row 2
+    chr1[x,]  = c1
+    chr2[x,] = c2
+    x+=2
+
+genoPloidy = np.stack([chr1,chr2], axis=2)
 
 
-for X, y in data
-    print(f"Shape of X: {X.shape}")
-    print(f"Shape of y: {y.shape} {y.dtype}")
-    break
+params = tf.convert_to_tensor(genoPloidy)
+
+
+
+
