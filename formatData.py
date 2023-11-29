ed5c54699861d2d08f732921a5642d10781ae7da
Isabella C. Penney <95446790+isabellajchiara@users.noreply.github.com>
2023-11-29 11:45:16 -0500

    create 3D tensor

--- formatData.py
+++ formatData.py
@@ -2,8 +2,13 @@ geno = pd.read_csv("unstructuredGeno.csv")
 resp = pd.read_csv("unstructuredPheno.csv")
 data = resp.join(geno)
 
+nLoci = 3570
+nInd = len(geno0/2
+ploidy = 2
+
 train = torch.tensor(data.values, dtype=torch.float)
 x = train[:,0]
+geno3D = X.reshape(nLoci, nInd,ploidy)
 y = train[:,1:3548]
 
 
