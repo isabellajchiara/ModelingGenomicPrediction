93aff56f61d3181f678242490dba82f0911aec74
Isabella (Chiaravallotti) Penney <95446790+isabellajchiara@users.noreply.github.com>
2024-02-28 10:55:14 -0500

    feature reduction

--- formatData.py
+++ formatData.py
@@ -7,15 +7,19 @@ val = 1
 chr = 1
 genotypes = []
 
+os.chdir('/home/ich/projects/def-haricots/ich/CDBN/genotypes')
+
 for file in os.listdir('/home/ich/projects/def-haricots/ich/CDBN/genotypes'):
-    file_name, file_ext = os.path.splitext(f)
+    file_name, file_ext = os.path.splitext(file)
     with open(f'chr{chr}.txt', 'r') as geno:
       geno = geno.read()
       array = pd.read_csv(io.StringIO(geno), sep='\s+')
       chr +=1
     genotypes.append(array)
     val += 1
-    
+
+os.chdir('/home/ich/projects/def-haricots/ich/CDBN')
+ 
 geno = pd.concat(genotypes) #huge DF containing all SNPs for all genotypes
 geno['taxa'] = geno['taxa'].str[0:8] #make geno entry names match pheno entry names 
 
@@ -29,8 +33,18 @@ data = location.merge(geno, left_on='Seq_ID', right_on='taxa') #merge data to pu
 X = data.drop(['taxa','SY','DM','SW','Unnamed: 0','env','Seq_ID'],axis=1) #genotypes only
 Y = data['DM'] #response variable only
 
-print(Y.head())
+imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+imp.fit(X)
+X = pd.DataFrame(imp.transform(X))
+
+sel = VarianceThreshold(threshold=(.91 * (1 - .91)))  #remove cols with variance below 0.91
+X = sel.fit_transform(X)
+print(X.shape)
+
+
 
 xTrain, xTest, yTrain, yTest = train_test_split( X, Y, test_size=0.2)
+
+
 
 
