3aaf1e69837af96429a0d1482fcbb4fa3f2c17bd
Isabella (Chiaravallotti) Penney <95446790+isabellajchiara@users.noreply.github.com>
2024-04-11 13:54:01 -0400

    file cleanup prior to showcase

--- Transformer/transformerEval.py
+++
@@ -1,9 +0,0 @@
-transformer.eval()
-
-# Generate random sample validation data
-
-with torch.no_grad():
-
-    val_output = transformer(genoValid, phenoValid)
-    val_loss = criterion(val_output.contiguous().view(-1, tgt_vocab_size), val_tgt_data[:, 1:].contiguous().view(-1))
-    print(f"Validation Loss: {val_loss.item()}")
