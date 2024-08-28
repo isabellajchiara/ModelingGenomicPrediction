11ffae3b41cfb7d00847450ddba703f92358ab07
Isabella (Chiaravallotti) Penney <95446790+isabellajchiara@users.noreply.github.com>
2024-08-28 14:44:11 -0400

    Update transformerBlocks.py

--- Transformer/transformerBlocks.py
+++ Transformer/transformerBlocks.py
@@ -1,4 +1,23 @@
 
+class CDBNDataset(torch.utils.data.Dataset):
+  '''
+  Prepare the CDBN dataset for regression
+  '''
+
+  def __init__(self, X, y, scale_data=True):
+    if not torch.is_tensor(X) and not torch.is_tensor(y):
+      # Apply scaling if necessary
+      if scale_data:
+          X = StandardScaler().fit_transform(X)
+      self.X = torch.from_numpy(X)
+      self.y = torch.from_numpy(y)
+
+  def __len__(self):
+      return len(self.X)
+
+  def __getitem__(self, i):
+      return self.X[i], self.y[i]
+
 
 #attention between each pair of positions in a sequence 
 
