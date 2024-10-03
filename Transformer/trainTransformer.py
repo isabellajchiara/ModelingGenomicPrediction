b64d9da1cd8e391a7415bd53c9b9a44235f00e61
Isabella (Chiaravallotti) Penney <95446790+isabellajchiara@users.noreply.github.com>
2024-10-03 08:21:36 -0400

    5 fold CV

--- Transformer/trainTransformer.py
+++ Transformer/trainTransformer.py
@@ -2,79 +2,83 @@ exec(open("dependencies.py").read())
 exec(open("transformerBlocks.py").read())
 exec(open("transformerBuild.py").read())
 
-data = pd.read_csv("SYdataTF.csv")
+data = pd.read_csv("tokenTrainingSY.csv")
 
-testing = data[data["1"]== "ID"]
-training = data[data["1"] != "ID"]
+X = data.drop(['Unnamed: 0','2','3'], axis=1)
+y = data['3']
 
-testing = testing.drop(["1"],axis=1) #remove loc column
-training = training.drop(["1"],axis=1)
+nSNPs = X.shape[1] 
 
-nSNPs = len(data.columns) -3
-
-
-xTrain = training.drop(["0","Unnamed: 0"], axis=1)
-xTest = testing.drop(["0","Unnamed: 0"],axis=1)
-xTrain = xTrain.replace({0.0:-1,int(1.0):0,2.0:int(1)})
-xTest = xTest.replace({0.0:-1,int(1.0):0,2.0:int(1)})
-
-yTrain = training["0"]
-yTest = testing["0"]
+accuracies = {} #Store accuracies for eac k fold
+kf = KFold(n_splits=5, shuffle=True, random_state=100) #cross validate to evaluate k values
 
 
-xTrain = np.array(xTrain)
-yTrain = np.array(yTrain)
-xTest = np.array(xTest)
-yTest = np.array(yTest)
+# 5-fold cross-validation
+for train_index, test_index in kf.split(X):
 
-xTrain = torch.from_numpy(xTrain)
-xTest = torch.from_numpy(xTest)
-yTrain = torch.from_numpy(yTrain)
-yTest = torch.from_numpy(yTest)
+    # train rest split for k fold
+    xTrain, xTest = X.iloc[train_index], X.iloc[test_index]
+    yTrain, yTest = y.iloc[train_index], y.iloc[test_index]
 
-src_vocab_size = nSNPs
-tgt_vocab_size = 1
-d_model = 512
-num_heads = 8
-num_layers = 6
-d_ff = 20
-max_seq_length = 100
-dropout = 0.1
+    xTrain = torch.tensor(xTrain.values)
+    yTrain = torch.tensor(yTrain.values)
+    xTest = torch.tensor(xTest.values)
+    yTest = torch.tensor(yTest.values)  
 
-transformer = Transformer(src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout)
+    src_vocab_size = nSNPs
+    tgt_vocab_size = 1
+    d_model = 512
+    num_heads = 8
+    num_layers = 6
+    d_ff = 2048
+    max_seq_length = X.shape[1]
+    dropout = 0.1
 
-# Convert src_data and tgt_data to LongTensor
-src_data = xTrain.long() 
-tgt_data = yTrain.unsqueeze(1).long() 
-src_data = torch.clamp(xTrain.long(), 0, src_vocab_size - 1) # Clamp values to be within vocabulary range
-tgt_data = torch.clamp(yTrain.unsqueeze(1).long(), 0, tgt_vocab_size - 1)
+    #define model
+    transformer = Transformer(src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout)
+
+    #define loss and optimizer 
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(transformer.parameters(), lr=0.01)
+
+    # train mode
+    transformer.train()
+
+    #train loop
+    for epoch in range(100):
+
+        optimizer.zero_grad()
+        output = transformer(xTrain, yTrain.long().unsqueeze(1)) 
+
+        loss = criterion(output.contiguous().view(-1, pheno_size), yTrain[:, 0].contiguous().view(-1).int())
+        loss.backward()
+        optimizer.step()
+        print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
+
+    print("Training Complete")
+
+    #save model
+    torch.save(transformer,"transformer.pth")
+
+    #eval mode
+    transformer.eval()
+
+    #determine accuracy for k fold
+    with torch.no_grad():
+
+        prediction = transformer(xTest, yTest)
+        print(f"Validation Loss: {val_loss.item()}")
 
 
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(transformer.parameters(), lr=0.1)
+    prediction = prediction.detach().numpy()
+    df_val_output = pd.DataFrame(val_output_np, columns=['val_output'])
+    correlation = yTest['0'].corr(df_val_output['val_output'])
 
-transformer.train()
 
-for epoch in range(100):
-    optimizer.zero_grad()
-    output = transformer(src_data, tgt_data)
-    # Make sure tgt_data is of the correct shape and type
-    loss = criterion(output.contiguous().view(-1, tgt_vocab_size), tgt_data[:, 0].contiguous().view(-1).float()) 
-    loss.backward()
-    optimizer.step()
-    print(f"Epoch: {epoch+1}, Loss: {loss.item()}")
+    accuracy = np.corrcoef(yPred, yTest)[0, 1]
+    accuracies.append(correlation)
 
-print("Training Complete")
+result = accuracies.mean()
 
-torch.save(transformer,"transformer.pth")
-
-transformer.eval()
-
-xTest = torch.clamp(xTest.long(), 0, src_vocab_size - 1) # Clamp values to be within vocabulary range
-yTest = torch.clamp(yTest.unsqueeze(1).long(), 0, tgt_vocab_size - 1)
-
-with torch.no_grad():
-
-    val_output = transformer(xTest, yTest)
-    val_loss = criterion(val_output.contiguous().view(-1, tgt_vocab_size), yTest.contiguous().view(-1))
-    print(f"Validation Loss: {val_loss.item()}")
+result.to_csv('SYresults.csv', index=False)
+#
