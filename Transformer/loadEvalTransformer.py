2368fb21c282d7dbc3b3eba7d2eb37ab65a76e48
Isabella (Chiaravallotti) Penney <95446790+isabellajchiara@users.noreply.github.com>
2025-01-17 15:51:49 -0500

    update to evaluate yield

--- Transformer/loadEvalTransformer.py
+++ Transformer/loadEvalTransformer.py
@@ -3,19 +3,19 @@ exec(open("dependencies.py").read())
 exec(open("transformerBlocks.py").read())
 exec(open("transformerBuild.py").read())
 
-data = pd.read_csv("DF_Data.csv")
+data = pd.read_csv("SY_Data.csv")
 
-X = data.drop(['Unnamed: 0','IDS','Days to flowering'], axis=1)
-y = data['Days to flowering']
+X = data.drop(['Unnamed: 0','IDS','Yield'], axis=1)
+y = data['Yield']
 
 stacked = X.stack().unique()
 unique = stacked.shape[0]
 
 src_vocab_size = int(unique)
 tgt_vocab_size = 1
-d_model = 250
-num_heads = 1
-num_layers = 2
+d_model = 100
+num_heads = 2
+num_layers = 5
 d_ff = 100
 max_seq_length = X.shape[1]
 dropout = 0.05
@@ -25,7 +25,7 @@ dropout = 0.05
 transformer = Transformer(src_vocab_size, tgt_vocab_size, d_model, num_heads, num_layers, d_ff, max_seq_length, dropout)
 
 #load the pretrained weights 
-transformer = torch.load("transformerDF.pth")
+transformer = torch.load("transformerSY.pth")
 
 #put in eval mode
 transformer.eval()
@@ -38,5 +38,6 @@ with torch.no_grad():
     prediction = transformer(Xtensor)
 
 accuracy = pearsonr(prediction,y)
+accuracy
 
 
